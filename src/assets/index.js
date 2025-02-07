ce272b3756ed422662ebd118566fd05d46a8d107
Sai Manikanta <123723630+saimani3@users.noreply.github.com>
2025-02-07 19:39:58 +0530

    added

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo1 from "./logo1.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import database from "./database.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import Localville from "./company/Localville.png";
+import css from "./tech/css.png";
+import btech from "./company/btech.png";
+import html from "./tech/html.png";
+import bootstrap from "./tech/bootstrap.jpg";
+import sql from "./tech/sql.jpg";
+import javascript from "./tech/javascript.png";
+import java from "./tech/java.jpg";
+import manual from "./tech/manual.jpg";
+import python from "./tech/python.jpg";
+import selenium from "./tech/selenium.jpg";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import qspiders from "./company/qspiders.png";
+import postman from "./tech/postman.jpg";
+import Hospital from "./Hospital.png";
+import ProductReviews from "./Product Reviews.png";
+import ecommerce from "./ecommerce.png";
+import tour from "./tour.png";
+import inter from "./company/inter.png";
+import ProfilePic from "./Profilepic.png";
+export {
+  logo1,
+  backend,
+  database,
+  creator,
+  Localville,
+  ProductReviews,
+  tour,
+  inter,
+  ProfilePic,
+  ecommerce,
+  qspiders,
+  mobile,
+  web,
+  github,
+  btech,
+  menu,
+  close,
+  Hospital,
+  css,
+  bootstrap,
+  sql,
+  postman,
+  selenium,
+  java,
+  python,
+  manual,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+};
